b10411ca19484b0ae9e47f52d97be65e0b6debd1
jwlh00 <juunshuu00@gmail.com>
2023-04-24 11:08:43 -0600

    Divide and Conquer update

--- DaC_LCS.py
+++ DaC_LCS.py
@@ -1,20 +1,49 @@
-def lcs_divide_and_conquer(X, Y):
-    # Base case: if one of the strings is empty, return an empty string as LCS
-    if len(X) == 0 or len(Y) == 0:
-        return ""
-    
-    # Divide: Encontrar el centro de cada string
-    midX = len(X) // 2
-    midY = len(Y) // 2
-    
-    # Conquer: Encontrar recursivamente los LCS de cada par de subcadenas
-    if X[midX] == Y[midY]:
-        # Si los caracteres de las mitades coinciden se incluyen en el LCS resultante
-        return lcs_divide_and_conquer(X[:midX], Y[:midY]) + X[midX] + lcs_divide_and_conquer(X[midX+1:], Y[midY+1:])
-    else:
-        # Sino, el LCS muestra dos posible sub solucion y presenta la mas larga
-        return max(lcs_divide_and_conquer(X[:midX], Y), lcs_divide_and_conquer(X, Y[:midY]), key=len)
+import sys
 
-# result = lcs_divide_and_conquer('Computer Science', 'Comp-Sci')
-# print(result)
-# print('de longitud:', len(result))
+def lcs_divide_and_conquer(s1, s2):
+    """
+    This function takes two strings s1 and s2 as input and returns the longest common substring between them using the divide and conquer approach.
+    """
+    def lcs_helper(s1, s2, l1, r1, l2, r2):
+        if l1 > r1 or l2 > r2:
+            return ""
+        if r1 - l1 == 0:
+            if s1[l1] in s2[l2:r2+1]:
+                return s1[l1]
+            else:
+                return ""
+        mid1 = (l1 + r1) // 2
+        mid2 = (l2 + r2) // 2
+        left_lcs = lcs_helper(s1, s2, l1, mid1, l2, mid2)
+        right_lcs = lcs_helper(s1, s2, mid1+1, r1, mid2+1, r2)
+        cross_lcs = find_cross_lcs(s1, s2, l1, r1, l2, r2)
+        if len(left_lcs) >= len(right_lcs) and len(left_lcs) >= len(cross_lcs):
+            return left_lcs
+        elif len(right_lcs) >= len(left_lcs) and len(right_lcs) >= len(cross_lcs):
+            return right_lcs
+        else:
+            return cross_lcs
+
+    def find_cross_lcs(s1, s2, l1, r1, l2, r2):
+        mid1 = (l1 + r1) // 2
+        mid2 = (l2 + r2) // 2
+        max_len = 0
+        lcs = ""
+        i = mid1
+        while i >= l1:
+            j = mid2
+            while j <= r2:
+                if s1[i] == s2[j]:
+                    k = 0
+                    while i-k >= l1 and j-k >= l2 and s1[i-k] == s2[j-k]:
+                        k += 1
+                    if k > max_len:
+                        max_len = k
+                        lcs = s1[i-k+1:i+1]
+                j += 1
+            i -= 1
+        return lcs
+
+    n1 = len(s1)
+    n2 = len(s2)
+    return lcs_helper(s1, s2, 0, n1-1, 0, n2-1)
