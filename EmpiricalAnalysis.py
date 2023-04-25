99fb46aea786a3c598e08595d5c7e845f0f133cb
jwlh00 <juunshuu00@gmail.com>
2023-04-25 09:24:12 -0600

    Empirical Analysis update

--- EmpiricalAnalysis.py
+++ EmpiricalAnalysis.py
@@ -2,13 +2,25 @@ from DaC_LCS import *
 from DP_LCS import *
 import time
 import random
+import string
 import matplotlib.pyplot as plt
 
-input_strings = {
-    'Computer Science': 'Comp-Sci',
-    'GeeksforGeeks': 'GeeksQuiz',
-    'OldSite:GeeksforGeeks.org': 'NewSite:GeeksQuiz.com'
-}
+def generate_random_string(length):
+    return ''.join(random.choice('ABCDEF') for _ in range(length))
+
+def insert_substring(string1, string2, substring):
+    index1 = random.randint(0, len(string1))
+    index2 = random.randint(0, len(string2))
+    return string1[:index1] + substring + string1[index1:], string2[:index2] + substring + string2[index2:]
+
+string_range = [10,20,40,80,160,320,640]
+input_strings = {}
+for i in string_range:
+    x = generate_random_string(i)
+    y = generate_random_string(i)
+    common_substring = generate_random_string(random.randint(1, i//2))
+    x, y = insert_substring(x, y, common_substring)
+    input_strings[x] = y
 
 lcs_divide_and_conquer_runtimes = []
 LCSubStr_runtimes = []
@@ -32,12 +44,13 @@ for x, y in input_strings.items():
     print("DynamicProgramming Length: ", LCSubStr(x, y, len(x), len(y)))
     print("===================")
 
-plt.plot(input_strings.keys(), lcs_divide_and_conquer_runtimes, label='Divide and Conquer')
-plt.plot(input_strings.keys(), LCSubStr_runtimes, label='Dynamic Programming')
-plt.xlabel('Input String')
+plt.plot(string_range, lcs_divide_and_conquer_runtimes, label='Divide and Conquer')
+plt.plot(string_range, LCSubStr_runtimes, label='Dynamic Programming')
+plt.xlabel('Input String Length')
 plt.ylabel('Time (seconds)')
 plt.legend()
 plt.show()
 
-print(LCSubStr_runtimes)
 print(lcs_divide_and_conquer_runtimes)
+print(LCSubStr_runtimes)
+    
