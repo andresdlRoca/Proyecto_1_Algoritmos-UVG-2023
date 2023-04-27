d415e69b076c58e35d6e3c108b2ea5d4d6a113eb
jwlh00 <60154594+jwlh00@users.noreply.github.com>
2023-04-27 14:51:47 -0500

    Update EmpiricalAnalysis.py

--- EmpiricalAnalysis.py
+++ EmpiricalAnalysis.py
@@ -5,6 +5,7 @@ import random
 import string
 import matplotlib.pyplot as plt
 
+string_range = [10,20,40,80,160,320,640]
 def generate_random_string(length):
     return ''.join(random.choice('ABCDEF') for _ in range(length))
 
@@ -13,13 +14,10 @@ def insert_substring(string1, string2, substring):
     index2 = random.randint(0, len(string2))
     return string1[:index1] + substring + string1[index1:], string2[:index2] + substring + string2[index2:]
 
-string_range = [10,20,40,80,160,320,640]
 input_strings = {}
 for i in string_range:
     x = generate_random_string(i)
     y = generate_random_string(i)
-    common_substring = generate_random_string(random.randint(1, i//2))
-    x, y = insert_substring(x, y, common_substring)
     input_strings[x] = y
 
 lcs_divide_and_conquer_runtimes = []
@@ -35,14 +33,6 @@ for x, y in input_strings.items():
     LCSubStr(x, y, len(x), len(y))
     end_time = time.perf_counter()
     LCSubStr_runtimes.append("{:.8f}".format(end_time - start_time))
-
-    print("x: ", x)
-    print("y: ", y)
-    ("---------------------")
-    print("substring: ", lcs_divide_and_conquer(x, y))
-    print("DivideAndConquer Length: ", len(lcs_divide_and_conquer(x, y)))
-    print("DynamicProgramming Length: ", LCSubStr(x, y, len(x), len(y)))
-    print("===================")
 
 plt.plot(string_range, lcs_divide_and_conquer_runtimes, label='Divide and Conquer')
 plt.plot(string_range, LCSubStr_runtimes, label='Dynamic Programming')
